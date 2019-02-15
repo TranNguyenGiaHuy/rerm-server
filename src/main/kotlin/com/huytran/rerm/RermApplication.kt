efe9978950aa647ab0f79b2ed1a7582069468ba5
Huy Tran <15520318@gm.uit.edu.vn>
2019-02-15 16:44:50 +0700

    init

---
+++ src/main/kotlin/com/huytran/rerm/RermApplication.kt
@@ -0,0 +1,12 @@
+package com.huytran.rerm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RermApplication
+
+fun main(args: Array<String>) {
+    runApplication<RermApplication>(*args)
+}
+
